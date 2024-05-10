c51d6bb28ab7a5e9f5c331f553f8bd5b53b071a9
erfan taheri <erfanta197@gmail.com>
2024-05-10 22:28:04 +0330

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -31,3 +31,17 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue);
 }
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) return NextResponse.json("invalid issue id");
+
+  await prisma.issue.delete({ where: { id: parseInt(params.id) } });
+
+  return NextResponse.json("deleted");
+}
