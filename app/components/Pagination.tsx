311ba8395db86a51b2039aeb2d297a170b0b2e68
erfan taheri <erfanta197@gmail.com>
2024-05-22 21:12:54 +0330

    Implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useSearchParams, useRouter } from "next/navigation";
 import {
   RxChevronLeft,
   RxChevronRight,
@@ -13,24 +15,52 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const { push } = useRouter();
+  const searchParams = useSearchParams();
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null;
+
+  const ChangePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    push("?" + params.toString());
+  };
 
   return (
     <Flex align="center" gap="2">
       <Text size="2">
         page {currentPage} of {pageCount}
       </Text>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => ChangePage(1)}
+      >
         <RxDoubleArrowLeft />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => ChangePage(currentPage - 1)}
+      >
         <RxChevronLeft />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => ChangePage(currentPage + 1)}
+      >
         <RxChevronRight />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => ChangePage(pageCount)}
+      >
         <RxDoubleArrowRight />
       </Button>
     </Flex>
